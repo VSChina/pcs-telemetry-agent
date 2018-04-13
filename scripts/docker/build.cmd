d86e5112b2008b602496786f8d82ac1f8899efcb
Erich Wang <eriwan@microsoft.com>
2018-04-13 18:25:31 +0800

    update script

--- scripts/docker/build.cmd
+++ scripts/docker/build.cmd
@@ -1,7 +1,7 @@
 @ECHO off & setlocal enableextensions enabledelayedexpansion
 
 :: Note: use lowercase names for the Docker images
-SET DOCKER_IMAGE=azureiotpcs/telemetry-agent-dotnet
+SET DOCKER_IMAGE=vschinaiot/pcs-telemetry-agent
 :: "testing" is the latest dev build, usually matching the code in the "master" branch
 SET DOCKER_TAG=%DOCKER_IMAGE%:testing
 
--- scripts/docker/publish.cmd
+++ scripts/docker/publish.cmd
@@ -1,9 +1,11 @@
 @ECHO off & setlocal enableextensions enabledelayedexpansion
 
 :: Note: use lowercase names for the Docker images
-SET DOCKER_IMAGE="azureiotpcs/telemetry-agent-dotnet"
+SET DOCKER_IMAGE="vschinaiot/pcs-telemetry-agent"
 :: "testing" is the latest dev build, usually matching the code in the "master" branch
 SET DOCKER_TAG=%DOCKER_IMAGE%:testing
+
+docker login -u %DOCKER_USERNAME% -p %DOCKER_PASSWORD%
 
 docker push %DOCKER_TAG%
 
